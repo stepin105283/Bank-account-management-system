90fda2d7f8af2edeca9be5048d10b624bd79bb32
stepin105283 <69103016+stepin105283@users.noreply.github.com>
2020-10-07 06:48:24 +0530

    Update header.h

--- implementation/inc/header.h
+++ implementation/inc/header.h
@@ -1,1 +1,16 @@
-
+#include<stdio.h>
+#include<string.h>
+struct date
+{
+    int date,month,year;
+};
+struct b
+{
+    int acc_no;
+    double phone_no;
+    char acc_name[30];
+    char branch_name[30];
+    float acc_bal;
+    struct date dob;
+}
+bank,find,up,with;
